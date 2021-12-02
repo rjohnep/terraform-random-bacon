24024a14c513b0ecfc80ae10ab85fb02137601ed
Eugene Rogach <i.am.r.john@gmail.com>
2021-12-02 10:11:46 +0100

    test changes

--- main.tf
+++ main.tf
@@ -2,10 +2,7 @@ resource "random_pet" "server" {
   keepers = {
     version = "1"
   }
-}
-
-resource "random_pet1" "server" {
-  keepers = {
-    version = "1"
+  keepers1 = {
+    version = "2"
   }
 }
