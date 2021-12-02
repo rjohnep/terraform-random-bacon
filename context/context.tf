8a8584e688ccc5c6595cbe1bae29c95d71645421
Eugene Rogach <i.am.r.john@gmail.com>
2021-12-02 10:26:55 +0100

    new tf

---
+++ context/context.tf
@@ -0,0 +1,21 @@
+resource "spacelift_context" "this" {
+  name = "Context ${var.number}"
+}
+
+resource "spacelift_environment_variable" "this" {
+  count = 50
+
+  context_id = spacelift_context.this.id
+  name       = "VALUE ${count.index}"
+  value      = "NUMBER ${count.index}"
+  write_only = count.index % 2 == 0
+}
+
+resource "spacelift_mounted_file" "this" {
+  count = 50
+
+  context_id    = spacelift_context.this.id
+  relative_path = "file-${count.index}.txt"
+  content       = "NUMBER ${count.index}"
+  write_only    = count.index % 2 == 0
+}
