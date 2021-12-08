123fbbf4cf9f089b098329e40452ac695f7011d1
Eugene Rogach <i.am.r.john@gmail.com>
2021-12-08 15:00:09 +0100

    long names test

--- context/context.tf
+++ context/context.tf
@@ -5,7 +5,7 @@ resource "spacelift_context" "this" {
 resource "spacelift_environment_variable" "this" {
   count = 50
 
-  context_id = spacelift_context.this.id
+  context_id = "${spacelift_context.this.id}-loooooooooooooooooooooooooooooooong--loooooooooooooooooooooooooooooooong--loooooooooooooooooooooooooooooooong"
   name       = "VALUE ${count.index}"
   value      = "NUMBER ${count.index}"
   write_only = count.index % 2 == 0
@@ -14,7 +14,7 @@ resource "spacelift_environment_variable" "this" {
 resource "spacelift_mounted_file" "this" {
   count = 50
 
-  context_id    = spacelift_context.this.id
+  context_id = "${spacelift_context.this.id}-loooooooooooooooooooooooooooooooong--loooooooooooooooooooooooooooooooong--loooooooooooooooooooooooooooooooong"
   relative_path = "file-${count.index}.txt"
   content       = "NUMBER ${count.index}"
   write_only    = count.index % 2 == 0
