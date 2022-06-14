1446ae6687c5058f0e5f400922064a11e79037b9
Eugene Rogach <i.am.r.john@gmail.com>
2022-06-14 15:42:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
-module "context" {
-  count = 3
+provider "spacelift" {
+}
 
-  source = "./context"
-  number = count.index
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift.io/spacelift-io/spacelift"
+    }
+  }
+}
+
+resource "spacelift_stack" "testowy-stack" {
+  name = "testowy"
+
+  administrative    = true
+  autodeploy        = false
+  branch            = "master"
+  description       = "Fooooo"
+  repository        = "b4k3r-test"
+  terraform_version = "0.12.27"
 }
